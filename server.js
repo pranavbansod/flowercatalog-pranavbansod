e79b2647778454c4020341409861bec51a0931c9
Pranav Bansod <pranav.bansod.14@gmail.com>
2018-01-09 16:12:52 +0530

    Added user login page and logout by url feature. but anyone can comment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const timeStamp = require('./time.js').timeStamp;
 const http = require('http');
 const WebApp = require('./webapp');
 
+let registered_users = [{userName:'prateikp',name:'Pratik Patel'}, {userName:'pranavb',name:'Pranav Bansod'}];
 let allGuestData = JSON.parse(fs.readFileSync('./data/allGuestData.json'));
 
 const getContentType = function(filename) {
@@ -80,7 +81,38 @@ const fileServer = function(req,res) {
   res.end();
 };
 
+let loadUser = (req,res)=>{
+  let sessionid = req.cookies.sessionid;
+  let user = registered_users.find(u=>u.sessionid==sessionid);
+  if(sessionid && user){
+    req.user = user;
+  }
+};
+
+
 let app = WebApp.create();
+
+
+app.post('/login.html',(req,res)=>{
+  let user = registered_users.find(u=>u.userName==req.body.userName);
+  if(!user) {
+    res.setHeader('Set-Cookie',`logInFailed=true`);
+    res.redirect('/login.html');
+    return;
+  }
+  let sessionid = new Date().getTime();
+  res.setHeader('Set-Cookie',`sessionid=${sessionid}`);
+  user.sessionid = sessionid;
+  res.redirect('/index.html');
+});
+
+app.get('/logout.html',(req,res)=>{
+  res.setHeader('Set-Cookie',[`loginFailed=false,Expires=${new Date(1).toUTCString()}`,`sessionid=0,Expires=${new Date(1).toUTCString()}`]);
+  delete req.user.sessionid;
+  res.redirect('/login');
+});
+
+
 app.addPreProcessor(logRequest);
 app.addPostProcessor(fileServer);
 
