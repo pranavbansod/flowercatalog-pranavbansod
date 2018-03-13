9f0d07d7e2f472a0a162eec74995a9377f58ddae
Pranav Bansod <pranav.bansod.14@gmail.com>
2018-03-13 11:48:16 +0530

    added package.json

--- server.js
+++ server.js
@@ -128,7 +128,7 @@ app.addPostProcessor(fileServer);
 
 
 
-const PORT = 8000;
+const PORT = process.env.PORT||8000;
 let server = http.createServer(app);
 server.on('error',e=>console.error('**error**',e.message));
 server.listen(PORT,(e)=>console.log(`server listening at ${PORT}`));
